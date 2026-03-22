bbbccc665659c9794dd6c59328e2f0366ded76ea
j2fyi <207806977+j2fyi@users.noreply.github.com>
2026-03-22 15:03:55 -0400

    chore: use custom components in webapp

--- apps/webapp/vite.config.ts
+++ apps/webapp/vite.config.ts
@@ -1,15 +1,15 @@
-
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { tanstackRouter } from '@tanstack/router-plugin/vite'
-
+import tailwindcss from "@tailwindcss/vite";
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 export default defineConfig({
 	plugins: [
+		tailwindcss(),
 		tanstackRouter({
-			target: 'react',
+			target: "react",
 			autoCodeSplitting: true,
 		}),
 		react(),
 	],
-})
+});
