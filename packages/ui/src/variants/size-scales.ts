4ffb63fcce550e3c4a0dffa3035a69c8f71a56f7
j2fyi <207806977+j2fyi@users.noreply.github.com>
2026-03-24 11:32:07 -0400

    chore: update interactive size scale styles to be a bit more roomy

--- packages/ui/src/variants/size-scales.ts
+++ packages/ui/src/variants/size-scales.ts
@@ -2,27 +2,27 @@ import { tw } from "../utils/tw.ts";
 
 export const interactiveSizes = {
 	1: tw(
-		"h-6 px-4",
+		"h-8 px-4",
 		"text-[length:var(--text-1)]",
 		"leading-[var(--text-1--line-height)]",
-		"rounded-[var(--radius-1)] gap-1",
+		"rounded-[var(--radius-1)] gap-1.5",
 	),
 	2: tw(
-		"h-8 px-5",
+		"h-10 px-5",
 		"text-[length:var(--text-2)]",
 		"leading-[var(--text-2--line-height)]",
-		"rounded-[var(--radius-2)] gap-1.5",
+		"rounded-[var(--radius-2)] gap-2",
 	),
 	3: tw(
-		"h-10 px-4",
+		"h-12 px-6",
 		"text-[length:var(--text-3)]",
 		"leading-[var(--text-3--line-height)]",
-		"rounded-[var(--radius-3)] gap-2",
+		"rounded-[var(--radius-3)] gap-2.5",
 	),
 	4: tw(
-		"h-12 px-5",
+		"h-14 px-7",
 		"text-[length:var(--text-4)]",
 		"leading-[var(--text-4--line-height)]",
-		"rounded-[var(--radius-4)] gap-2.5",
+		"rounded-[var(--radius-4)] gap-3",
 	),
 } as const;
