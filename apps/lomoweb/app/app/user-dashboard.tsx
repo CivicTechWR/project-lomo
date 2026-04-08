70348ace3b4e03fea13efdbe41543d310333029a
j2fyi <207806977+j2fyi@users.noreply.github.com>
2026-04-08 19:34:49 -0400

    chore: fast commit

--- apps/lomoweb/app/app/user-dashboard.tsx
+++ apps/lomoweb/app/app/user-dashboard.tsx
@@ -19,7 +19,7 @@ export function UserDashboard({
 	const router = useRouter();
 	const user = usePreloadedAuthQuery(preloadedUser);
 
-	if (!user || user === "No USER FOUND") {
+	if (!user) {
 		return null;
 	}
 
